1a2bb22a129388b91dcac86788788885fce779e5
Ruslik <rockband1992@ukr.net>
2013-10-14 21:43:11 +0300

    First commit

---
+++ gen/com/example/psyhology_tests/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.psyhology_tests;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
