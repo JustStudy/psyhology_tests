612ecf0912c98fe29cc924dcde5eabe9e260d901
Ruslik <rockband1992@ukr.net>
2013-10-25 00:47:59 +0300

    fixed some bugs

--- src/com/example/psyhology_tests/MainStartActivity.java
+++ src/com/example/psyhology_tests/MainStartActivity.java
@@ -46,69 +46,7 @@ public class MainStartActivity extends MainRelativeActivity {
         tx.commit();
 
 
-
-        // enable ActionBar app icon to behave as action to toggle nav drawer
-       //  getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-      //   getSupportActionBar().setHomeButtonEnabled(true);
-
-
-        /*mDrawerLayout.setDrawerListener(mDrawerToggle);
-        if (savedInstanceState == null) {
-           fTrans.replace(R.id.content_frame,fragmentTestList);
-        }*/
     }
 
-  /*  @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
 
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    public void replace(Fragment fr) {
-
-        tx.replace(R.id.content_frame, fr);
-    }*/
-
-   /* @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        tx = getSupportFragmentManager().beginTransaction();
-        switch (item.getItemId()) {
-
-            case R.id.item2:
-                // Toast.makeText(getApplicationContext(),"item1",Toast.LENGTH_LONG).show();
-                 DrawerState=mDrawerLayout.isDrawerOpen(mDrawerList);
-                if(DrawerState){
-                mDrawerLayout.closeDrawer(mDrawerList);
-            }
-                if(!DrawerState) {
-                    mDrawerLayout.openDrawer(mDrawerList);
-                }
-                //tx.replace(R.id.content_frame, Fragment.instantiate(MainStartActivity.this, "com.example.psyhology_tests.FragmentTestList")).commit();
-
-                //  replace(Fragment.instantiate(MainStartActivity.this, "com.example.psyhology_tests.FragmentTestList"));
-                return true;
-            case R.id.item1:
-                //Toast.makeText(getApplicationContext(),"item2",Toast.LENGTH_LONG).show();
-                if(!getResources().getBoolean(R.bool.istablet)){
-                    tx.replace(R.id.content_frame, Fragment.instantiate(MainStartActivity.this, "com.example.psyhology_tests.StartFragment")).commit();
-                }
-              else  tx.replace(R.id.fragment2ForTablet, Fragment.instantiate(MainStartActivity.this, "com.example.psyhology_tests.StartFragment")).commit();
-                // replace(Fragment.instantiate(MainStartActivity.this, "com.example.psyhology_tests.FragmentTestAction"));
-                return true;
-            case android.R.id.home:
-                DrawerState=mDrawerLayout.isDrawerOpen(mDrawerList);
-                if(DrawerState){
-                    mDrawerLayout.closeDrawer(mDrawerList);
-                }
-                if(!DrawerState) {
-                    mDrawerLayout.openDrawer(mDrawerList);
-                }
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-
-    }*/
 }
